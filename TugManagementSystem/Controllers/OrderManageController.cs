f553eb4b2c23a8c8c068cef705b9d0938c74351a
garyquiet <liugang_nj@163.com>
2016-06-05 15:33:27 +0800

    左边菜单

--- TugManagementSystem/Controllers/OrderManageController.cs
+++ TugManagementSystem/Controllers/OrderManageController.cs
@@ -10,6 +10,7 @@ namespace TugManagementSystem.Controllers
     public class OrderManageController : BaseController
     {
         #region 页面Action
+
         // GET: OrderManage
         public ActionResult OrderManage(string lan, int? id)
         {
@@ -19,7 +20,6 @@ namespace TugManagementSystem.Controllers
             return View();
         }
 
-
         //GET: OrderScheduling
         public ActionResult OrderScheduling(string lan, int? id)
         {
@@ -28,14 +28,14 @@ namespace TugManagementSystem.Controllers
 
             return View();
         }
-        #endregion
 
+        #endregion 页面Action
 
         #region 订单管理页面Action
+
         public ActionResult GetData(bool _search, string sidx, string sord, int page, int rows)
         {
             this.Internationalization();
-
 
             //string s = Request.QueryString[6];
 
@@ -50,7 +50,6 @@ namespace TugManagementSystem.Controllers
 
                 List<OrderInfor> page_orders = orders.Skip((page - 1) * rows).Take(rows).OrderBy(u => u.IDX).ToList<OrderInfor>();
 
-
                 var jsonData = new { page = page, records = totalRecordNum, total = totalPageNum, rows = page_orders };
                 return Json(jsonData, JsonRequestBehavior.AllowGet);
             }
@@ -63,7 +62,6 @@ namespace TugManagementSystem.Controllers
         public ActionResult GetDataForLoadOnce(bool _search, string sidx, string sord, int page, int rows)
         {
             this.Internationalization();
-
 
             //string s = Request.QueryString[6];
 
@@ -78,7 +76,6 @@ namespace TugManagementSystem.Controllers
 
                 //List<OrderInfor> page_orders = orders.Skip((page - 1) * rows).Take(rows).OrderBy(u => u.IDX).ToList<OrderInfor>();
 
-
                 var jsonData = new { page = page, records = totalRecordNum, total = totalPageNum, rows = orders };
                 return Json(jsonData, JsonRequestBehavior.AllowGet);
             }
@@ -88,13 +85,12 @@ namespace TugManagementSystem.Controllers
             }
         }
 
-
         public ActionResult AddEdit()
         {
             this.Internationalization();
 
-
             #region Add
+
             if (Request.Form["oper"].Equals("add"))
             {
                 try
@@ -116,7 +112,6 @@ namespace TugManagementSystem.Controllers
                         aOrder.LinkMan = Request.Form["LinkMan"];
                         aOrder.LinkPhone = Request.Form["LinkPhone"];
                         aOrder.LinkEmail = Request.Form["LinkEmail"];
-
 
                         if (Request.Form["BigTugNum"] != "")
                             aOrder.BigTugNum = Convert.ToInt32(Request.Form["BigTugNum"]);
@@ -168,11 +163,12 @@ namespace TugManagementSystem.Controllers
                     //Response.Write(@Resources.Common.EXCEPTION_MESSAGE);
                     return Json(ret);
                 }
-
             }
-            #endregion
+
+            #endregion Add
 
             #region Edit
+
             if (Request.Form["oper"].Equals("edit"))
             {
                 try
@@ -191,7 +187,7 @@ namespace TugManagementSystem.Controllers
                         aOrder.CustomerID = Convert.ToInt32(Request.Form["CustomerID"]);
                         aOrder.CustomerName = Request.Form["CustomerName"];
                         aOrder.LastUpDate = DateTime.Now.ToString("yyyy-MM-dd");
-                        
+
                         aOrder.LinkMan = Request.Form["LinkMan"];
                         aOrder.LinkPhone = Request.Form["LinkPhone"];
                         aOrder.LinkEmail = Request.Form["LinkEmail"];
@@ -203,22 +199,22 @@ namespace TugManagementSystem.Controllers
                         aOrder.ShipID = Convert.ToInt32(Request.Form["ShipID"]);
                         aOrder.ShipName = Request.Form["ShipName"];
                         if (Request.Form["BigTugNum"] != "")
-                        aOrder.BigTugNum = Convert.ToInt32(Request.Form["BigTugNum"]);
+                            aOrder.BigTugNum = Convert.ToInt32(Request.Form["BigTugNum"]);
                         if (Request.Form["MiddleTugNum"] != "")
-                        aOrder.MiddleTugNum = Convert.ToInt32(Request.Form["MiddleTugNum"]);
+                            aOrder.MiddleTugNum = Convert.ToInt32(Request.Form["MiddleTugNum"]);
                         if (Request.Form["SmallTugNum"] != "")
-                        aOrder.SmallTugNum = Convert.ToInt32(Request.Form["SmallTugNum"]);
+                            aOrder.SmallTugNum = Convert.ToInt32(Request.Form["SmallTugNum"]);
                         aOrder.WorkPlace = Request.Form["WorkPlace"];
                         aOrder.WorkStateID = Convert.ToInt32(Request.Form["WorkStateName"]);//Convert.ToInt32(Request.Form["WorkStateID"]);
                         aOrder.WorkStateName = Request.Form["WorkStateID"];
                         aOrder.Remark = Request.Form["Remark"];
-                        
+
                         aOrder.UserDefinedCol1 = Request.Form["UserDefinedCol1"];
                         aOrder.UserDefinedCol2 = Request.Form["UserDefinedCol2"];
                         aOrder.UserDefinedCol3 = Request.Form["UserDefinedCol3"];
                         aOrder.UserDefinedCol4 = Request.Form["UserDefinedCol4"];
 
-                        if(Request.Form["UserDefinedCol5"] != "")
+                        if (Request.Form["UserDefinedCol5"] != "")
                             aOrder.UserDefinedCol5 = Convert.ToDouble(Request.Form["UserDefinedCol5"]);
 
                         if (Request.Form["UserDefinedCol6"] != "")
@@ -238,19 +234,17 @@ namespace TugManagementSystem.Controllers
 
                         return Json(new { code = Resources.Common.SUCCESS_CODE, message = Resources.Common.SUCCESS_MESSAGE });
                     }
-
                 }
                 catch (Exception exp)
                 {
-                    
                     return Json(new { code = Resources.Common.EXCEPTION_CODE, message = Resources.Common.EXCEPTION_MESSAGE });
                 }
             }
-            #endregion
 
-            return Json(new { code = Resources.Common.ERROR_CODE, message = Resources.Common.ERROR_MESSAGE});
+            #endregion Edit
+
+            return Json(new { code = Resources.Common.ERROR_CODE, message = Resources.Common.ERROR_MESSAGE });
         }
-
 
         public ActionResult Delete()
         {
@@ -279,19 +273,16 @@ namespace TugManagementSystem.Controllers
             {
                 return Json(new { code = Resources.Common.EXCEPTION_CODE, message = Resources.Common.EXCEPTION_MESSAGE });
             }
-
         }
-
 
         public ActionResult GetCustomer(string term)
         {
-
             List<object> source = new List<object>();
             source.Add(new { CustomerID = "123", ShipName = "abc" });
             source.Add(new { CustomerID = "234", ShipName = "cde" });
             source.Add(new { CustomerID = "345", ShipName = "efg" });
             source.Add(new { CustomerID = "456", ShipName = "ghi" });
-            
+
             var p = Request.Params;
 
             List<object> list = new List<object>();
@@ -303,26 +294,20 @@ namespace TugManagementSystem.Controllers
             return Json(jsonData, JsonRequestBehavior.AllowGet);
         }
 
-
         public string GetCustomField(string CustomName)
         {
-            string[] query = { "未审核","已确认", "备货中", "已发货", "已完成"};
+            string[] query = { "未审核", "已确认", "备货中", "已发货", "已完成" };
 
             string s = "<select>";
-            for(int i = 0; i < query.Length; i++)
+            for (int i = 0; i < query.Length; i++)
             {
-                s += string.Format("<option value={0}>{1}</option>", i+1, query[i]);
+                s += string.Format("<option value={0}>{1}</option>", i + 1, query[i]);
             }
             s += "</select>";
 
             return s;
         }
 
-        #endregion
-
-
-        #region 订单调度页面Action
-        #endregion
-
+        #endregion 订单管理页面Action
     }
 }
