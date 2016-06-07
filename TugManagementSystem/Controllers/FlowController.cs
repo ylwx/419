9c605d7a6ef4a96ba4ababa28fa7669b311599a4
ytx <shipways@163.com>
2016-06-07 19:26:36 +0800

    创建流程

---
+++ TugManagementSystem/Controllers/FlowController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TugManagementSystem.Controllers
+{
+    public class FlowController : BaseController
+    {
+        public ActionResult CreateFlow()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Flow/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
